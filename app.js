144b5f8ecd702ee9d8ed326a5f14e208df3bdc0f
Juan Chemello <juanchemell@gmail.com>
2020-07-02 15:55:58 -0300

    data, methods, data binding, events

---
+++ app.js
@@ -0,0 +1,20 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    age: 31,
+    x: 0,
+    y: 0,
+  },
+  methods: {
+    add: function (value = 1) {
+      this.age += value;
+    },
+    subtract: function (value = 1) {
+      this.age -= value;
+    },
+    updateXY: function (event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    },
+  },
+});
