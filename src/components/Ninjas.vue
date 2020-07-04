fbe1d8aed2f507b7242b57a24d0c4edead07504d
Juan Chemello <juanchemell@gmail.com>
2020-07-04 17:18:57 -0300

    component-css

--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -1,19 +1,21 @@
 <template>
-  <ul>
-    <li v-for="(ninja, index) in ninjas" :key="index">{{ ninja }}</li>
-  </ul>
+  <div>
+    <h1>List of ninjas</h1>
+    <ul>
+      <li v-for='(ninja, index) in ninjas' :key='index'>{{ ninja }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      ninjas: ['Yoshi', 'Mario', 'Ryu'],
-    };
+    return { ninjas: ['Luigi', 'Mario', 'Ryu'] };
   },
 };
 </script>
-
-<style>
-
+<style scoped>
+h1 {
+  color: green
+}
 </style>
