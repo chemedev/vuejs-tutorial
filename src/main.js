1723ce1a6feb375011ac4bc70e0904fe789f3be9
Juan Chemello <juanchemell@gmail.com>
2020-07-05 11:28:33 -0300

    custom-directives

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-vars */
 import Vue from 'vue';
 import VueResource from 'vue-resource';
 import App from './App.vue';
 
 Vue.use(VueResource);
+
+// Custom directives
+Vue.directive('rainbow', {
+  bind(el, binding, vnode) {
+    el.style.color = `#${Math.random().toString().slice(2, 8)}`;
+  },
+});
 
 Vue.config.productionTip = false;
 
