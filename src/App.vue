2cd1632989bb6917ea64cb7df132c1177df19c33
Juan Chemello <juanchemell@gmail.com>
2020-07-04 22:19:33 -0300

    slots

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,39 @@
 <template>
   <div>
-    <app-header :propTitle="title" />
-    <app-ninjas :ninjas="ninjas" />
-    <app-footer :propTitle="title" />
+    <form-helper>
+      <div slot="form-header">
+        <h3>This is the title of the form</h3>
+        <p>Information about the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required />
+        <input type="password" placeholder="password" required />
+      </div>
+      <div slot="form-controls">
+        <button @click="handleSubmit">Submit</button>
+      </div>
+    </form-helper>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-import Ninjas from './components/Ninjas.vue';
+import formHelper from './components/formHelper.vue';
 
 export default {
   components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-ninjas': Ninjas,
+    'form-helper': formHelper,
   },
   data() {
     return {
-      ninjas: [
-        { name: 'Ryu', speciality: 'Vue Components', show: false },
-        { name: 'Crystal', speciality: 'HTML Wizardry', show: false },
-        { name: 'Hitoshi', speciality: 'Click Events', show: false },
-        { name: 'Tango', speciality: 'Conditionals', show: false },
-        { name: 'Kami', speciality: 'Webpack', show: false },
-        { name: 'Yoshi', speciality: 'Data Diggin', show: false },
-      ],
-      title: 'Vue Ninjas',
+      title: 'I am a dynamic slot title',
     };
   },
-  methods: {},
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      console.log('AAAAAA');
+    },
+  },
 };
 </script>
 
