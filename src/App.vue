d239cdd176b35d2584abd589168c5b7d4cd4a033
Juan Chemello <juanchemell@gmail.com>
2020-07-04 22:33:14 -0300

    dynamic-components

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,28 @@
 <template>
   <div>
-    <form-helper>
-      <div slot="form-header">
-        <h3>This is the title of the form</h3>
-        <p>Information about the form</p>
-      </div>
-      <div slot="form-fields">
-        <input type="text" placeholder="name" required />
-        <input type="password" placeholder="password" required />
-      </div>
-      <div slot="form-controls">
-        <button @click="handleSubmit">Submit</button>
-      </div>
-    </form-helper>
+    <keep-alive>
+      <component :is="component" />
+    </keep-alive>
+    <button @click="component = 'form-one'">Show form one</button>
+    <button @click="component = 'form-two'">Show form two</button>
   </div>
 </template>
 
 <script>
-import formHelper from './components/formHelper.vue';
+import formOne from './components/formOne.vue';
+import formTwo from './components/formTwo.vue';
 
 export default {
   components: {
-    'form-helper': formHelper,
+    'form-one': formOne,
+    'form-two': formTwo,
   },
   data() {
     return {
-      title: 'I am a dynamic slot title',
+      component: 'form-one',
     };
   },
-  methods: {
-    handleSubmit(e) {
-      e.preventDefault();
-      console.log('AAAAAA');
-    },
-  },
+  methods: {},
 };
 </script>
 
